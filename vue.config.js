c3917e4e20fcabaa131a976b297f61bf56482fbb
Till Sanders <till.sanders@finanzritter.com>
2018-08-20 18:12:03 +0100

    Improve PWA

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  pwa: {
+    name: '3 in 2',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+  },
+};
